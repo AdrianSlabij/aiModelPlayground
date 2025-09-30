feb8f9c11530965c89bf464697e535517cf49332
AdrianSlabij <aslabij@gmail.com>
2025-09-29 20:52:57 -0400

    fix: added debugging statements for database connection logging

--- backend/src/prisma.service.ts
+++ backend/src/prisma.service.ts
@@ -1,17 +1,15 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
-/**
- * This service extends the PrismaClient and handles the database connection.
- * By using NestJS's OnModuleInit lifecycle hook, it ensures that a connection
- * is established as soon as the application module is initialized.
- * This service is then injected into other services that need to perform
- * database operations.
- */
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    await this.$connect();
+    console.log('Attempting to connect to the database...');
+    try {
+      await this.$connect();
+      console.log('Database connection established successfully!');
+    } catch (error) {
+      console.error('Failed to connect to the database.', error);
+    }
   }
 }
-
